c0d6617b4e2e974a59c1803587adb40a82961092
Tamuzo77 <tamuzomenga@gmail.com>
2024-03-29 16:17:25 +0100

    GRB Page

--- app/Filament/Clusters/Settings.php
+++ app/Filament/Clusters/Settings.php
@@ -9,7 +9,7 @@ class Settings extends Cluster
     //    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
     protected static ?string $navigationLabel = 'Généraux';
 
-    protected static ?int $navigationSort = 100;
+    protected static ?int $navigationSort = 1;
 
     protected static ?string $navigationGroup = 'Paramètres';
 }
