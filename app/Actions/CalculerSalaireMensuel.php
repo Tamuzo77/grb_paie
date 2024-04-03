83c0fc4c76b93389ad5e1463ca688f1d798ca0fc
Tamuzo77 <tamuzomenga@gmail.com>
2024-04-03 14:41:40 +0100

    avance paiement control

--- app/Actions/CalculerSalaireMensuel.php
+++ app/Actions/CalculerSalaireMensuel.php
@@ -20,4 +20,14 @@ class CalculerSalaireMensuel
 
         return $result;
     }
+
+    public static function nbreJoursTravaille(\App\Models\Employee $employee)
+    {
+        $nb_jours_absences = 0;
+        foreach ($employee->absences()->where('date_debut', '>=', now()->startOfMonth())->whereDeductible(true)->get() as $absence) {
+            $nb_jours_absences += date_diff($absence->date_debut, $absence->date_fin)->days;
+        }
+        return 20 - $nb_jours_absences;
+
+    }
 }
--- app/Filament/Resources/ClientResource/RelationManagers/EmployeesRelationManager.php
+++ app/Filament/Resources/ClientResource/RelationManagers/EmployeesRelationManager.php
@@ -248,10 +248,10 @@ class EmployeesRelationManager extends RelationManager
                                     ->hidden(fn (Forms\Get $get) => $get('type_paiement_id') == TypePaiement::SALAIRE)
                                     ->maxValue(function (Employee $record, Forms\Get $get) {
                                         if ($get('type_paiement_id') == TypePaiement::AVANCE) {
-                                            return $record->salaire;
+                                            return $record->salaire / 2;
                                         }
                                     })
-                                    ->default(fn (Employee $record) => $record->salaire)
+                                    ->default(5000)
                                     ->suffix('FCFA'),
                                 Forms\Components\Select::make('mode_paiement_id')
                                     ->label('Mode de paiement')
@@ -266,7 +266,6 @@ class EmployeesRelationManager extends RelationManager
                                 Forms\Components\Select::make('type_paiement_id')
                                     ->label('Type de paiement')
                                     ->required()
-                                    ->columnSpan(2)
                                     ->searchable()
                                     ->live(onBlur: true)
                                     ->preload()
@@ -279,14 +278,28 @@ class EmployeesRelationManager extends RelationManager
                                     ->optionsLimit(3),
                                 Forms\Components\DateTimePicker::make('date_debut')
                                     ->helperText('Intervalle du service payé')
+                                    ->hidden()
                                     ->columnSpan(2)
                                     ->label('Date de début'),
                                 Forms\Components\DateTimePicker::make('date_fin')
                                     ->helperText('Intervalle du service payé')
+                                    ->hidden()
                                     ->columnSpan(2)
                                     ->label('Date de fin'),
+                                Forms\Components\TextInput::make('nb_jours_travaille')
+                                    ->numeric()
+                                    ->label('Jours travaillés dans le mois')
+                                    ->default(function (Employee $record) {
+                                        return CalculerSalaireMensuel::nbreJoursTravaille($record);
+                                    } )
+                                    ->required(),
+                                Forms\Components\TextInput::make('pas')
+                                    ->visible(fn (Forms\Get $get) => $get('type_paiement_id') == TypePaiement::PRET)
+                                    ->columnSpan(2)
+                                    ->helperText('Echelonner le paiement')
+                                    ->numeric()
 
-                            ])->columns(4),
+                            ])->columns(2),
                         //                        $this->getContentSection(),
 
                     ])
