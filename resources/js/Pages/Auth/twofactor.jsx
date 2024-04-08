55f06decbf0f5bcdab4651f10868b045578a0499
bai <esperanceahouissou@gmail.com>
2024-04-08 14:18:14 +0100

    page intermediaire

--- resources/js/Pages/Auth/twofactor.jsx
+++ resources/js/Pages/Auth/twofactor.jsx
@@ -94,7 +94,7 @@ const TwoFactor = ({ status }) => {
             <div className='flex items-center justify-center mt-4'>
                 <Link
                     href={route('verify.resend')}
-                    className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none"
                 >
                     Renvoyez le code
                 </Link>
