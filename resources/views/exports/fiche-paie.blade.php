ab7aab70a189607074f02f153a1ea338f6fabe7a
bai <esperanceahouissou@gmail.com>
2024-04-08 10:43:14 +0100

    style terminer

--- resources/views/exports/fiche-paie.blade.php
+++ resources/views/exports/fiche-paie.blade.php
@@ -1,97 +1,174 @@
-<table class='w-full table-auto'>
-    <thead class='text-center bg-primary'>
-        <tr class='pl-3'>
-            <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="9"></td>
-            <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="6">
+<table>
+    <thead >
+        <tr>
+            <td style="height:35px"></td>
+            <td style="height:50px;border:2px solid #3498db;vertical-align:middle;font-weight:bold;text-align:center;" colspan="14">REPUBLIQUE DU BENIN</td>
+        </tr>
+        <tr>
+            <td style="height:35px"></td>
+            <td style="height:50px;border:2px solid #3498db;text-align:center;vertical-align:middle;font-weight:bold;text-align:center;" colspan="4">CMFD Sarl</td>
+            <td style="height:50px;border:2px solid #3498db;vertical-align:middle;font-weight:bold;text-align:center;" colspan="10"></td>
+        </tr>
+        
+        <tr>
+            <td style="height:35px"></td>
+            <td style="height:50px;vertical-align:middle;border:2px solid #3498db;text-align:center;font-size:18px;font-weight:bold;" colspan="14">FICHE DE PAIE</td>
+        </tr>
+        <tr>
+            <td style="height:35px"></td>
+            <td style="height:30px;vertical-align:middle;border:2px solid #3498db" colspan="9"></td>
+            <td style="height:30px;vertical-align:middle;border:2px solid #3498db" colspan="5">
                 <h3>Fait le 12 mars 2024</h3>
         </tr>
         <tr>
-            <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="15" class='py-3'>xxxxxx</td>
+            <td style="height:30px"></td>
+            <td style="height:30px" colspan="9"></td>
+            <td style="height:30px;vertical-align:middle;border:2px solid #3498db" colspan="5" class='py-3'>xxxxxx</td>
         </tr>
         <tr class="">
-            <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="6">xxxxx</td>
+            <td style="height:30px;"></td>
+            <td style="height:30px;vertical-align:middle;border:2px solid #3498db" colspan="9"></td>
+            <td style="height:30px;vertical-align:middle;border:2px solid #3498db" colspan="5">xxxxx</td>
         </tr>
-   
+        <tr class="">
+            <td style="height:35px"></td>
+            <td style="height:35px" colspan="9"></td>
+            <td style="background-color: #3498db;color: #ffffff;height:35px;vertical-align:middle;font-weight:bold;" colspan="5">Cotonou Rép.Du Bénin</td>
+        </tr>
+        <tr>
+            <td style="height:35px"></td>
+            <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="14" class='py-3'></td>
+        </tr>
     </thead>
 
     <tbody>
         <tr>
-            <td>Nom : xxxxxxx</td>
+            <td style="height:35px"></td>
+            <td colspan="6" style="background-color: #3498db;color: #ffffff;height:35px;vertical-align:middle;font-weight:bold;">Nom : xxxxxxx</td>
+            <td colspan="4"style="background-color: #3498db;color: #ffffff;height:35px;vertical-align:middle;font-weight:bold;">Prénoms:</td>
+            <td colspan="4"style="background-color: #3498db;color: #ffffff;height:35px;vertical-align:middle;font-weight:bold;">xxxxxxx</td>
         </tr>
         <tr>
-            <td>Fonction : xxxxxxx</td>
+            <td style="height:35px"></td>
+            <td colspan="6"style="background-color: #3498db;color: #ffffff;height:>35px;vertical-align:middle;font-weight:bold;">Fonction : xxxxxxx</td>
+            <td colspan="4"style="background-color: #3498db;color: #ffffff;height:>35px;vertical-align:middle;font-weight:bold;">Catégorie: </td>
+            <td colspan="4"style="background-color: #3498db;color: #ffffff;height:>35px;vertical-align:middle;font-weight:bold;">Agent d'Exécution</td>
         </tr>
-        <tr>Situation matrimoniale</tr>
+        <tr>
+            <td style="height:35px"></td>
+            <td colspan="6"style="background-color: #3498db;color: #ffffff;height:>35px;vertical-align:middle;font-weight:bold;">Situation matrimoniale</td>
+            <td colspan="4"style="background-color: #3498db;color: #ffffff;height:>35px;vertical-align:middle;font-weight:bold;">d'enfants: xxxxxxx</td>
+            <td colspan="4"style="background-color: #3498db;color: #ffffff;height:>35px;vertical-align:middle;font-weight:bold;"></td>
+        </tr>
     <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="15" class='py-3'></td>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="14" class='py-3'></td>
+    </tr>
+    {{-- <td >
+
+
+        <tr>d'enfants: xxxxxxx</tr>
+      </td>
+      <td colSpan={2} className=''>
+
+
+        <tr>
+      <tr colSpan={5} className='py-3'></tr>
+    </tr>
+      </td> --}}
+    <tr class="">
+        <td style="height:50px"></td>
+        <td style="background-color: #3498db;color: #ffffff;height:50px;vertical-align:middle;font-weight:bold;" colspan="10">Eléments</td>
+        <td style="background-color: #3498db;color: #ffffff;height:50px;vertical-align:middle;font-weight:bold;text-align:center" colspan="4">Montants</td>
+    </tr>
+    <tr>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="10">SALAIRES BRUTES</td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db;text-align:center"colspan="4">74060</td>
+
     </tr>
     <tr class="">
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="3">Eléments</td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="3"></td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="3"></td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="3"></td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="3" class='text-center'>Montants</td>
-
+        <td style="height:50px"></td>
+        <td style="background-color: #3498db;color: #ffffff;height:50px;vertical-align:middle;font-weight:bold;" colspan="2"></td>
+        <td style="background-color: #3498db;color: #ffffff;height:50px;vertical-align:middle;font-weight:bold;" colspan="8">SALAIRE BRUT MENSUEL</td>
+        <td style="background-color: #3498db;color: #ffffff;height:50px;vertical-align:middle;font-weight:bold;text-align:center" colspan="4">74060</td>
     </tr>
     <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="12">SALAIRES BRUTES</td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" class='text-center'>74060</td>
-
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="10">RETENUES OBLIGATOIRES</td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db;text-align:center" colspan="4">XXXXX</td>
+    </tr>
+    <tr>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="10">C.N.S.S</td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db;text-align:center" colspan="4">XXXXX</td>
+    </tr>
+    <tr>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="10">I.P.T.S</td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db;text-align:center" colspan="4">XXXXX</td>
+    </tr>
+    <tr>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="10">AVANCE SUR SALAIRE</td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db;text-align:center" colspan="4">XXXXX</td>
     </tr>
     <tr class="">
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db"></td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="9">SALAIRE BRUT MENSUEL</td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" class='text-center'>74060</td>
+        <td style="height:50px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db;background-color: #3498db;"colspan="2"></td>
+        <td style="background-color: #3498db;color: #ffffff;height:50px;vertical-align:middle;font-weight:bold;" colspan="8">TOTAL RETENUES OBLIGATOIRES</td>
+        <td style="background-color: #3498db;color: #ffffff;height:50px;vertical-align:middle;font-weight:bold;text-align:center" colspan="4">4066</td>
     </tr>
     <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="12">RETENUES OBLIGATOIRES</td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" class='text-center'>XXXXX</td>
-    </tr>
-    <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="12">C.N.S.S</td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" class='text-center'>XXXXX</td>
-    </tr>
-    <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="12">I.P.T.S</td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" class='text-center'>XXXXX</td>
-    </tr>
-    <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="12">AVANCE SUR SALAIRE</td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" class='text-center'>XXXXX</td>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="14" class='py-3'></td>
     </tr>
     <tr class="">
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="9">TOTAL RETENUES OBLIGATOIRES</td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" class='text-center'>4066</td>
+        <td style="height:50px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db;background-color: #3498db;" colspan="2"></td>
+        <td style="background-color: #3498db;color: #ffffff;height:50px;vertical-align:middle;font-weight:bold;" colspan="8">SALAIRE NET A PAYER</td>
+        <td style="background-color: #3498db;color: #ffffff;height:50px;vertical-align:middle;font-weight:bold;text-align:center;" colspan="4">70000</td>
     </tr>
     <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="15" class='py-3'></td>
-    </tr>
-    <tr class="">
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db"></td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="6">SALAIRE NET A PAYER</td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db"></td>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" class='text-center'>70000</td>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db;font-weight:bold" colspan="14">MODE DE REGLEMENT DE:</td>
     </tr>
     <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="15">
-
-            <p>SOIXANTE DIX MILLE</p>
-        </td>
-
-
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db;font-weight:bold" colspan="14">SOIXANTE DIX MILLE</td>
     </tr>
     <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db">Caisse</td>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="2">Caisse</td>
         <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="12"></td>
     </tr>
     <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db">Chèque</td>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="2">Chèque</td>
         <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="12">X</td>
     </tr>
     <tr>
-        <td style="height:35px;vertical-align:middle;border:2px solid #3498db">Virement</td>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="2">Virement</td>
         <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="12"></td>
     </tr>
-
+    <tr>
+        <td style="height:50px"></td>
+        <td style="height:50px;vertical-align:middle;border:2px solid #3498db;font-weight:bold;text-align:center;" colspan="14">DANS VOTRE INTERET ET POUR VALIDER VOS DROITS, VEILLER CONSERVER CETTE FICHE DE PAIE SANS LIMITATION DE DUREE</td>
+    </tr>
+    <tr>
+        <td style="height:50x;"></td>
+        <td style="height:50px;vertical-align:middle;border:2px solid #3498db" colspan="9"></td>
+        <td style="height:50px;vertical-align:middle;border:2px solid #3498db;text-align:center" colspan="5">La Directrice Générale</td>
+    </tr>
+    <tr>
+        <td style="height:35px"></td>
+        <td style="height:35px;vertical-align:middle;border:2px solid #3498db" colspan="14" class='py-3'></td>
+    </tr>
+    <tr >
+        <td style="height:50px;"></td>
+        <td style="height:50px;vertical-align:middle;border:2px solid #3498db" colspan="9"></td>
+        <td style="height:50px;vertical-align:middle;border:2px solid #3498db;font-weight:bold;text-align:center" colspan="5">xxxxx</td>
+    </tr>
     </tbody>
 </table>
